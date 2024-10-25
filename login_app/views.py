9451759fe5802f3f1dfa4e589e2576e7b552ad23
Thorgrim <fordelonemarco@orange.fr>
2024-10-25 17:25:51 +0200

    register fonctionnel

--- login_app/views.py
+++ login_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.contrib import messages
 
 def login_view(request):
@@ -27,4 +28,15 @@ def logout_view(request):
     return render(request, 'logout.html')
 
 def register_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Username already exists.")
+        else:
+            User.objects.create_user(username=username, password=password)
+            messages.success(request, f'Account created for {username}!')
+            return redirect('login')
+
     return render(request, 'register.html')
